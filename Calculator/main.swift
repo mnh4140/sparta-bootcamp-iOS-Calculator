27cdd41ef1b3a6369f2392c9e2627cc30fa4e068
NH <mnh4140@gmail.com>
2025-03-11 12:25:02 +0900

    refactor: 사칙연산 메소드를 각각의 class로 분리
    - 각 연산을 별도 클래스로 분리하여 Calculator 클래스의 책임을 줄임
    - Calculator 메소드들을 각각의 class로 구현
    - Calculator는 연산 클래스를 활용하도록 변경
    - Calculator 내부 코드에 연산 class 인스턴스 생성 및 사용

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -8,30 +8,12 @@
 import Foundation
 
 class Calculator {
-   // 더하기
-    func add(_ num1: Double, _ num2: Double) -> Double {
-       return num1 + num2
-   }
-   
-   // 빼기
-   func sub(_ num1: Double, _ num2: Double) -> Double {
-       return num1 - num2
-   }
-   
-   // 곱하기
-   func mul(_ num1: Double, _ num2: Double) -> Double {
-       return num1 * num2
-   }
-   
-   // 나누기
-    func div(_ num1: Double, _ num2: Double) -> Double? {
-        // guard 구문을 사용하여 0일때 예외처리
-        guard num2 != 0 else {
-            print("Error : 0으로 나눌 수 없습니다.")
-            return nil
-        }
-        return num1 / num2
-    }
+    // Lv3 클래스 관계 맺기
+    let addOperation: AddOperation = AddOperation()
+    let substractOperation: SubstractOperation = SubstractOperation()
+    let multiplyOperation: MultiplyOperation = MultiplyOperation()
+    let divideOperation: DivideOperation = DivideOperation()
+
     
     // Lv2 나머지 연산 추가
     func remainder(_ num1: Int, _ num2: Int) -> Int? {
@@ -44,37 +26,66 @@ class Calculator {
     }
 }
 
+// Lv3 클래스 추가
+class AddOperation {
+    func add(_ num1: Double, _ num2: Double) -> Double{
+        return num1 + num2
+    }
+}
+
+class SubstractOperation {
+    func sub(_ num1: Double, _ num2: Double) -> Double{
+        return num1 - num2
+    }
+}
+
+class MultiplyOperation {
+    func mul(_ num1: Double, _ num2: Double) -> Double{
+        return num1 * num2
+    }
+}
+
+class DivideOperation {
+    func div(_ num1: Double, _ num2: Double) -> Double? {
+        // guard 구문을 사용하여 0일때 예외처리
+        guard num2 != 0 else {
+            print("Error : 0으로 나눌 수 없습니다.")
+            return nil
+        }
+        return num1 / num2
+    }
+}
+
 
 
 let calculator = Calculator() // 인스턴스 생성하여 변수에 할당
 
-print("사직연산 결과")
+print("1. 사직연산 결과")
 
 // 사칙 연산 테스트
-let sum = calculator.add(10, 20)
-print(" > 더하기 결과:", sum)
+let sumReult = calculator.addOperation.add(11.2, 9.8)
+print(" > 더하기 결과:", sumReult) //  더하기 결과: 21.0
 
-let difference = calculator.sub(30, 20)
-print(" > 빼기 결과:", difference)
+let differenceResult = calculator.substractOperation.sub(1.4, 8.5)
+print(" > 빼기 결과:", differenceResult) // 빼기 결과: -7.1
 
-let product = calculator.mul(10, 20)
-print(" > 곱하기 결과:", product)
+let productResult = calculator.multiplyOperation.mul(3.3, 5.6)
+print(" > 곱하기 결과:", productResult) // 곱하기 결과: 18.479999999999997
 
-// if let을 사용해 옵셔널 언래핑
-if let quotient = calculator.div(60, 20) {
-    print(" > 나누기 결과:", quotient)
+if let quotientResult1 = calculator.divideOperation.div(96.4, 22) {
+    print(" > 나누기 결과:", quotientResult1) // 나누기 결과: 4.381818181818182
 }
 
 // Lv1 예외 상황: num2가 0일 때
-print("\n예외상황 처리 확인")
+print("\n2. 나눗셈 예외상황 처리 확인")
 
-// if let을 사용해 옵셔널 언래핑
-if let result1 = calculator.div(6, 0) {
-    print(" > 나누기 결과:", result1)  // 실행 안 됨 (에러 메시지만 출력)
+if let quotientResult2 = calculator.divideOperation.div(10, 0) {
+    print(" > 나누기 결과:", quotientResult2) // 실행 안 됨 (에러 메시지만 출력)
 }
 
+
 // Lv2 나머지 연산 테스트
-print("\n나머지 연산 테스트")
+print("\n3. 나머지 연산 테스트")
 
 if let result = calculator.remainder(6, 3) {
     print(" > 나머지 연산: \(result)") // 0
@@ -85,7 +96,7 @@ if let result = calculator.remainder(5, 3) {
 }
 
 // Lv2 예외처리 테스트
-print("\n예외상황 처리 확인")
+print("\n4. 나머지 연산 예외상황 처리 확인")
 
 if let result = calculator.remainder(6, 0) {
     print(" > 나머지 연산: \(result)") // 실행되지 않음 (에러 메시지 출력)
