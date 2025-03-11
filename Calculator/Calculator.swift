a4e1d405ff5b9facd85993ff64f4d5dfb6cee103
NH <mnh4140@gmail.com>
2025-03-11 17:47:11 +0900

    refactor: Calculator 클래스 내부 로직 변경
    - 프로토콜을 프로퍼티로 사용하지 않고 메소드의 매개변수로 사용
    - 이전 로직은 Calculator 인스턴스가 생성 시, 한가지 연산만 수행 가능
    - Calculator 인스턴스가 동적으로 연산을 선택할 수 있듬

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -6,15 +6,7 @@
 //
 
 class Calculator {
-    //
-    let operation: AbstractOperation?
-    
-    // 초기화
-    init(operation: AbstractOperation) {
-        self.operation = operation
-    }
-    
-    func calculation(_ num1: Double, _ num2: Double) -> Double? {
-        return operation?.calculation(num1, num2)
+    func calculation(_ operation: AbstractOperation, _ num1: Double, _ num2: Double) -> Double? {
+        return operation.calculation(num1, num2)
     }
 }
--- Calculator/main.swift
+++ Calculator/main.swift
@@ -7,20 +7,22 @@
 
 import Foundation
 
+let calculator: Calculator = Calculator()
+
 print("사칙연산 수행")
 
-if let addCalc = Calculator(operation: AddOperation()).calculation(10, 20) {
+if let addCalc = calculator.calculation(AddOperation() ,10, 20) {
     print(" > 덧셈\t: \(addCalc)")
 }
 
-if let subCalc = Calculator(operation: SubstractOperation()).calculation(10, 20) {
+if let subCalc = calculator.calculation(SubstractOperation(), 10, 20) {
     print(" > 뺄셈\t: \(subCalc)")
 }
 
-if let mulCalc = Calculator(operation: MultiplyOperation()).calculation(10, 20) {
+if let mulCalc = calculator.calculation(MultiplyOperation(), 10, 20) {
     print(" > 곱셈\t: \(mulCalc)")
 }
 
-if let divCalc = Calculator(operation: DivideOperation()).calculation(10, 20) {
+if let divCalc = calculator.calculation(DivideOperation() ,10, 20) {
     print(" > 나눗셈\t: \(divCalc)")
 }
