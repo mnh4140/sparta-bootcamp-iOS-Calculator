5f56d6a71d4f0d65259ddb35ebe596c5e76576be
NH <mnh4140@gmail.com>
2025-03-11 12:03:04 +0900

    feat: 메소드 div 예외처리 기능 추가
    - 0으로 나누려고 하면 Error를 출력하도록 예외처리 추가
    - guard 구문을 사용하여, 조기 종료 시킴

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -28,9 +28,14 @@ class Calculator {
    }
    
    // 나누기
-   func div(_ num1: Double, _ num2: Double) -> Double {
-       return num1 / num2
-   }
+    func div(_ num1: Double, _ num2: Double) -> Double? {
+        // guard 구문을 사용하여 0일때 예외처리
+        guard num2 != 0 else {
+            print("Error : 0으로 나눌 수 없습니다.")
+            return nil
+        }
+        return num1 / num2
+    }
 }
 
 let calculator = Calculator() // 인스턴스 생성하여 변수에 할당
@@ -49,4 +54,10 @@ print(" > 곱하기 결과:", product)
 
 let quotient = calculator.div(60, 20)
 print(" > 나누기 결과:", quotient)
+
+// 예외 상황: num2가 0일 때
+print("\n예외상황 처리 확인")
+
+let result1 = calculator.div(6, 0)
+print(" 나누기 결과:", result1)  // 실행 안 됨 (에러 메시지만 출력)
 
