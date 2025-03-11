bee0d7ac9772db44688289eb952fc4bffc002609
NH <mnh4140@gmail.com>
2025-03-11 12:15:30 +0900

    feat: Lv2 나머지 연산 기능 추가 및 예외처리 기능 추가
    - 나머지 연산 메소드 추가
    - 나머지 연산 메소드에서도 0으로 나눌 수 없게 guard 구문으로 예외처리 추가
    - 위 기능 테스트 구문 추가

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -7,10 +7,6 @@
 
 import Foundation
 
-// 1. Lv1 `03/10 까지`
-//     - [ ]  더하기, 빼기, 나누기, 곱하기 연산을 수행할 수 있는 Calculator 클래스를 만들기
-//     - [ ]  생성한 클래스를 이용하여 연산을 진행하고 출력
-
 class Calculator {
    // 더하기
     func add(_ num1: Double, _ num2: Double) -> Double {
@@ -36,7 +32,19 @@ class Calculator {
         }
         return num1 / num2
     }
+    
+    // Lv2 나머지 연산 추가
+    func remainder(_ num1: Int, _ num2: Int) -> Int? {
+        // guard 구문을 사용하여 0일때 예외처리
+        guard num2 != 0 else {
+            print("Error : 0으로 나머지 연산을 수행할 수 없습니다.")
+            return nil
+        }
+        return num1 % num2
+    }
 }
+
+
 
 let calculator = Calculator() // 인스턴스 생성하여 변수에 할당
 
@@ -56,10 +64,29 @@ print(" > 곱하기 결과:", product)
 if let quotient = calculator.div(60, 20) {
     print(" > 나누기 결과:", quotient)
 }
-// 예외 상황: num2가 0일 때
+
+// Lv1 예외 상황: num2가 0일 때
 print("\n예외상황 처리 확인")
 
 // if let을 사용해 옵셔널 언래핑
 if let result1 = calculator.div(6, 0) {
     print(" > 나누기 결과:", result1)  // 실행 안 됨 (에러 메시지만 출력)
+}
+
+// Lv2 나머지 연산 테스트
+print("\n나머지 연산 테스트")
+
+if let result = calculator.remainder(6, 3) {
+    print(" > 나머지 연산: \(result)") // 0
+}
+
+if let result = calculator.remainder(5, 3) {
+    print(" > 나머지 연산: \(result)") // 2
+}
+
+// Lv2 예외처리 테스트
+print("\n예외상황 처리 확인")
+
+if let result = calculator.remainder(6, 0) {
+    print(" > 나머지 연산: \(result)") // 실행되지 않음 (에러 메시지 출력)
 }
